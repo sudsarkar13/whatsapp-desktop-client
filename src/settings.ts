1d6fb1d447f5bf356c06be79bfbe4ed56f3a7967
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2025-04-27 12:37:24 +0530

    refactor ♻️: update yarn and enhance type safety
    - The main goal of the changes is to update the yarn installation state.
    - The main goal of the changes is to enhance type safety and functionality of the Settings class by specifying the generic type for the store and modifying the get method to properly handle default values.

--- src/settings.ts
+++ src/settings.ts
@@ -1,16 +1,16 @@
 import Store from "electron-store";
 
 export default class Settings {
-	private readonly store: Store;
+	private readonly store: Store<Record<string, any>>;
 	private readonly section: string;
 
 	constructor(section: string) {
-		this.store = new Store();
+		this.store = new Store<Record<string, any>>();
 		this.section = section + ".";
 	}
 
 	public get<T>(key: string, defaults: T | null = null): T {
-		return this.store.get(this.section + key) ?? defaults;
+		return this.store.get(this.section + key, defaults as T);
 	}
 
 	public set<T>(key: string, value: T): void {
