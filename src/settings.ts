4abd9ea1f85bfcd5c46844aeb4381fbe2cddab03
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2025-04-27 12:35:39 +0530

    refactor ♻️: updated dependencies and settings class
    - Updated yarn install state to reflect changes in package dependencies.
    - Added dependency for electron store type definitions to enhance development support.
    - The main goal of the changes is to improve the settings class by adding generic types and introducingnull-coalescing operator to handle default values more effectively.
    - The main goal of the changes is to update dependencies, specifically adding and upgrading electron-store and its corresponding type definitions to ensure compatibility and fix potential issues.

--- src/settings.ts
+++ src/settings.ts
@@ -1,18 +1,19 @@
-import Store from 'electron-store';
+import Store from "electron-store";
 
 export default class Settings {
-    private readonly store = new Store();
-    private readonly section: string;
+	private readonly store: Store;
+	private readonly section: string;
 
-    constructor(section: string) {
-        this.section = section + ".";
-    }
+	constructor(section: string) {
+		this.store = new Store();
+		this.section = section + ".";
+	}
 
-    public get(key: string, defaults: any = null): any {
-        return this.store.get(this.section + key, defaults);
-    }
+	public get<T>(key: string, defaults: T | null = null): T {
+		return this.store.get(this.section + key) ?? defaults;
+	}
 
-    public set(key: string, value: any): void {
-        this.store.set(this.section + key, value);
-    }
-};
+	public set<T>(key: string, value: T): void {
+		this.store.set(this.section + key, value);
+	}
+}
